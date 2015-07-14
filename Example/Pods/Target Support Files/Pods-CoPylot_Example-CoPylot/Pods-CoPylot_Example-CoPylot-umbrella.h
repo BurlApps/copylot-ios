6255ed5bcde20031e7219ddace7e4afe846247db
Brian Vallelunga <vallelungabrian@gmail.com>
2015-07-13 20:09:31 -0700

    first commit

---
+++ Example/Pods/Target Support Files/Pods-CoPylot_Example-CoPylot/Pods-CoPylot_Example-CoPylot-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double CoPylotVersionNumber;
+FOUNDATION_EXPORT const unsigned char CoPylotVersionString[];
+
---
+++ Example/Pods/Target Support Files/Pods-CoPylot_Example/Pods-CoPylot_Example-environment.h
@@ -0,0 +1,14 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// CoPylot
+#define COCOAPODS_POD_AVAILABLE_CoPylot
+#define COCOAPODS_VERSION_MAJOR_CoPylot 0
+#define COCOAPODS_VERSION_MINOR_CoPylot 1
+#define COCOAPODS_VERSION_PATCH_CoPylot 0
+
