e6a9c0c46fc7278981048fdc6bbd7b258738b805
Brian Vallelunga <vallelungabrian@gmail.com>
2015-07-13 19:58:51 -0700

    Initial commit

---
+++ Example/CoPylot/CPAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CPAppDelegate.h
+//  CoPylot
+//
+//  Created by Brian Vallelunga on 07/13/2015.
+//  Copyright (c) 2015 Brian Vallelunga. All rights reserved.
+//
+
+@import UIKit;
+
+@interface CPAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Example/CoPylot/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  CoPylot
+//
+//  Created by Brian Vallelunga on 07/13/2015.
+//  Copyright (c) 2015 Brian Vallelunga. All rights reserved.
+//
+
+@import UIKit;
+#import "CPAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([CPAppDelegate class]));
+    }
+}
